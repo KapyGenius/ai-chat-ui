c796269c9060e34700cbca29fbea1a489eb772b6
Donald Kapyouo <dkapyouo@gmail.com>
2026-07-19 00:06:42 +0200

    chore: update page title and modify Vite config
    - Changed the document title from "Pydantic AI" to "FH SWF Study Modules Agent".
    - Commented out the base URL configuration in Vite for build command.

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 const BACKEND_DEV_SERVER_PORT = process.env.BACKEND_PORT ?? 38001
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig(({  }) => ({
   plugins: [react(), tailwindcss(), tsconfigPaths({ root: __dirname })],
-  base: command === 'build' ? 'https://cdn.jsdelivr.net/npm/@pydantic/ai-chat-ui/dist/' : '',
+  // base: command === 'build' ? 'https://cdn.jsdelivr.net/npm/@pydantic/ai-chat-ui/dist/' : '',
   build: {
     assetsDir: 'assets',
   },
